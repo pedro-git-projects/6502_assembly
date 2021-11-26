e6fa2d13829313ec6258c5d0a197f6026bc542d2
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-11-26 13:29:08 -0300

    fixing by one bug

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -17,17 +17,17 @@ Start:
 ; That is, the entrire RAM and the whole TIA registers     					   ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	lda #0    ; A = 0
-	ldx #$FF   ; X = #$FF
+	ldx #$FF  ; X = #$FF
+	sta $FF   ; Making sure $FF is zeroed before the loop starts
 MemLoop: 
+	dex 		; X--
 	sta $0,X    ; Store the value of A inside memory address $0 + X
-	dex    ; X--
 	bne MemLoop ; X starts with #$FF while X is not loaded with 0, we branch back to MemLoop
 				; Another way to see this is: Loop until the z-flag is set
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 ; Fill the ROM size util it reaches 4KB 													;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 	org $FFFC
 	.word Start 		; Atari 2600 expect four bytes at the end of the cartridge
 	.word Start			; We can generate two of those bytes with the instruction .word
